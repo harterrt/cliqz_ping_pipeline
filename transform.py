0e36c94e5e922cadc341899c267b094feb5ea178
Ryan Harter <harterrt@gmail.com>
2017-02-01 23:29:58 +0000

    Simple dataframe conversion works, column names broken

--- transform.py
+++ transform.py
@@ -7,20 +7,28 @@ from moztelemetry import get_pings_properties
 from moztelemetry.dataset import Dataset
 
 
-class DataFrameConfig:
-    class ColumnConfig:
-        def __init__(self, name, path, cleaning_func, struct_type):
-            self.name = name
-            self.path = path
-            self.cleaning_func = cleaning_func
-            self.struct_type = struct_type
+class ColumnConfig:
+    def __init__(self, name, path, cleaning_func, struct_type):
+        self.name = name
+        self.path = path
+        self.cleaning_func = cleaning_func
+        self.struct_type = struct_type
 
+class DataFrameConfig:
     def __init__(self, col_configs):
         self.columns = [ColumnConfig(*col) for col in col_configs]
 
     def toStructType(self):
-        StructType(self.columns.map(
-            lambda col: StructField(col.name, col.struct_type, True)))
+        StructType(map(
+            lambda col: StructField(col.name, col.struct_type, True),
+            self.columns))
+
+    def get_names(self):
+        return map(lambda col: col.name, self.columns)
+
+    def get_paths(self):
+        return map(lambda col: col.path, self.columns)
+
 
 
 def pings_to_df(pings, data_frame_config):
@@ -33,7 +41,7 @@ def pings_to_df(pings, data_frame_config):
     def build_cell(ping, column_config):
         """Takes a json ping and a column config and returns a cleaned cell"""
         raw_value = ping[column_config.path]
-        func = column.cleaning_func
+        func = column_config.cleaning_func
         if func is not None:
             return func(raw_value)
         else:
@@ -42,11 +50,11 @@ def pings_to_df(pings, data_frame_config):
     def ping_to_row(ping):
         return [build_cell(ping, col) for col in data_frame_config.columns]
 
-    filtered_pings = get_pings_properties(pings, config.map(lambda x: x[1]))
+    filtered_pings = get_pings_properties(pings, data_frame_config.get_paths())
 
     return sqlContext.createDataFrame(
         filtered_pings.map(ping_to_row),
-        data_frame_config.toStructType)
+        schema = data_frame_config.toStructType())
 
 def __main__():
     get_doctype_pings = lambda docType: Dataset.from_source("telemetry") \
@@ -56,12 +64,12 @@ def __main__():
         .records(sc)
 
     df_config = DataFrameConfig([
-        ("client_id", "client_id", None, StringType()),
+        ("client_id", "clientId", None, StringType()),
         ("submission_date", "meta/submissionDate", None, StringType()),
-        ("creation_date", "creation_date", None, StringType()),
-        ("events", "payload/events", None, StringType())
+        ("creation_date", "creationDate", None, StringType()),
+        #("events", "payload/events", None, StringType())
     ])
 
-    return (get_doctpe_pings, df_config)
+    return (get_doctype_pings, df_config)
 
 
